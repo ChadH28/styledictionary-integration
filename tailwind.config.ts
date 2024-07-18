2161c56fbb01d23fada513d1da451bcd557136e9
Chad Hoosain <chad@bnry.digital>
2024-07-18 15:23:03 +0200

    added exported json to tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import * as tokens from "./styles/tokens.json";
 
 const config: Config = {
   content: [
@@ -8,9 +9,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "",
-        "gradient-conic": "",
+      backgroundColor: {
+        primary: tokens.background_color.primary,
+        secondary: tokens.background_color.secondary,
+      },
+      colors: {
+        black: tokens.colors.black,
+        white: tokens.colors.white,
+        orange100: tokens.colors.orange[100],
+      },
+      fontSize: {
+        small: tokens.text.sizes.large,
+        medium: tokens.text.sizes.medium,
+        large: tokens.text.sizes.small,
       },
     },
   },
